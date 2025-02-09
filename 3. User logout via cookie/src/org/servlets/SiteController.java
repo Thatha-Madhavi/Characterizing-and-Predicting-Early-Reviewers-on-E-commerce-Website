f8c0dece800723c7dd88684ee8bcec49d963340e
Madhavi Thatha <63993593+Thatha-Madhavi@users.noreply.github.com>
2025-02-08 23:06:18 -0500

    Add files via upload

---
+++ 3. User logout via cookie/src/org/servlets/SiteController.java
@@ -0,0 +1,65 @@
+package org.studyeasy.servlets;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ * Servlet implementation class SiteController
+ */
+@WebServlet("/SiteController")
+public class SiteController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#HttpServlet()
+	 */
+	public SiteController() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		String username = request.getParameter("username");
+		String password = request.getParameter("password");
+		
+		if(username.equals("studyeasy") && password.equals("Love")) {
+			//Invalidating session if any
+			request.getSession().invalidate();
+			HttpSession newSession = request.getSession(true);
+		    newSession.setMaxInactiveInterval(300);
+		    Cookie cUsername = new Cookie("username", username);
+		    response.addCookie(cUsername);
+		    response.sendRedirect("memberArea.jsp");
+			
+		}else {
+			response.sendRedirect("login.jsp");
+		}
+
+	}
+
+}
+
+
+
+
