6241bae5d3127039c690ac82e7fba9e12bae0ca9
Madhavi Thatha <63993593+Thatha-Madhavi@users.noreply.github.com>
2025-02-09 14:42:00 -0500

    Add files via upload

---
+++ 6. Fixing redirect and forward links/src/org/servlets/MemberAreaController.java
@@ -0,0 +1,58 @@
+package org.studyeasy.servlets;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ * Servlet implementation class MemberAreaController
+ */
+@WebServlet("/MemberAreaController")
+public class MemberAreaController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#HttpServlet()
+	 */
+	public MemberAreaController() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		String action = request.getParameter("action");
+		switch (action) {
+		case "destroy":
+			request.getSession().invalidate();		
+			response.sendRedirect(request.getContextPath()+"/SiteController?action=login");
+			break;
+		case "memberArea":
+			request.getRequestDispatcher("memberArea.jsp").forward(request, response);
+			break;
+		default:
+			 
+			break;
+		}
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
