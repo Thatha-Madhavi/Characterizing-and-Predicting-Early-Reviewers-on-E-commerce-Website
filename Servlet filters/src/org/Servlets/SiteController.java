3aa50fed1c632347e8d16103f46d481a614e5711
Madhavi Thatha <63993593+Thatha-Madhavi@users.noreply.github.com>
2025-02-09 15:19:29 -0500

    Add files via upload

---
+++ Servlet filters/src/org/Servlets/SiteController.java
@@ -0,0 +1,85 @@
+package org.studyeasy.servlets;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ * Servlet implementation class SiteController
+ */
+@WebServlet("/SiteController")
+public class SiteController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#HttpServlet()
+	 */
+	public SiteController() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		String action = request.getParameter("action");
+		switch (action) {
+		
+		case "login":
+			request.getRequestDispatcher("login.jsp").forward(request, response);
+			break;
+
+		default:
+			break;
+		}
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		String action = request.getParameter("action");
+		switch (action) {
+		case "loginSubmit":
+			authenticate(request, response);
+			break;
+
+		default:
+			break;
+		}
+
+	}
+	public void authenticate(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		String username = request.getParameter("username");
+		String password = request.getParameter("password");
+		
+		if(username.equals("studyeasy") && password.equals("Love")) {
+			//Invalidating session if any
+			request.getSession().invalidate();
+			HttpSession newSession = request.getSession(true);
+		    newSession.setMaxInactiveInterval(300);
+		    newSession.setAttribute("username", username);
+		    String encode = response.encodeURL(request.getContextPath());
+		    response.sendRedirect(encode+"/MemberAreaController?action=memberArea");
+			
+		}else {
+			response.sendRedirect(request.getContextPath()+"/SiteController?action=login");
+		}
+	}
+
+}
+
+
+
+
