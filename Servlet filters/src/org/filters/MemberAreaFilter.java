abf8d2dc009516b4733544206c413c0856bb5d1f
Madhavi Thatha <63993593+Thatha-Madhavi@users.noreply.github.com>
2025-02-09 15:18:40 -0500

    Add files via upload

---
+++ Servlet filters/src/org/filters/MemberAreaFilter.java
@@ -0,0 +1,56 @@
+package org.studyeasy.filters;
+
+import java.io.IOException;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet Filter implementation class MemberAreaFilter
+ */
+@WebFilter("/MemberAreaController")
+public class MemberAreaFilter implements Filter {
+
+	/**
+	 * Default constructor.
+	 */
+	public MemberAreaFilter() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @see Filter#destroy()
+	 */
+	public void destroy() {
+		// TODO Auto-generated method stub
+	}
+
+	/**
+	 * @see Filter#doFilter(ServletRequest, ServletResponse, FilterChain)
+	 */
+	public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain)
+			throws IOException, ServletException {
+		
+		HttpServletResponse response = (HttpServletResponse) res;
+		HttpServletRequest request =  (HttpServletRequest) req;
+		if(request.getSession().getAttribute("username")==null) {
+			response.sendRedirect(request.getContextPath()+"/SiteController?action=login");
+		}else {
+			chain.doFilter(request, response);
+		}
+	}
+
+	/**
+	 * @see Filter#init(FilterConfig)
+	 */
+	public void init(FilterConfig fConfig) throws ServletException {
+		// TODO Auto-generated method stub
+	}
+
+}
